2c6ed93f3b464443786709cb94124cfbeee9e3e9
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-03-11 01:15:44 +0530

    swagger

--- One_Two_Many_Relationship/index.js
+++ One_Two_Many_Relationship/index.js
@@ -1,12 +1,250 @@
 
 const express=require('express');
 const { connection } = require("./db");
-
+const swaggerJsDoc=require('swagger-jsdoc');
+const swaggerUi=require('swagger-ui-express');
 const {UserModel} = require('./model/user.models');
 const Post = require('./model/post.models');
 
 const app=express();
 app.use(express.json());
+
+
+const options={
+    definition:{
+        openapi:"3.0.0",
+        info:{
+            title:"User Management System",
+            version:"1.0.0"
+        },
+        servers:[
+            {
+                url:"http://localhost:4500"
+            }
+        ]
+    },
+    apis: ["./index.js"]
+}
+
+const openApiSpec=swaggerJsDoc(options);
+app.use('/apidocs',swaggerUi.serve,swaggerUi.setup(openApiSpec));
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the post
+ *         title:
+ *           type: string
+ *           description: The post title
+ *           required: true
+ *         content:
+ *           type: string
+ *           description: The post content
+ *           required: true
+ *         user:
+ *           type: string
+ *           description: The user ID who created the post
+ *           format: uuid
+ *           required: true
+ *
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the user
+ *         name:
+ *           type: string
+ *           description: The user name
+ *           required: true
+ *         email:
+ *           type: string
+ *           description: The user email
+ *           required: true
+ *         posts:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/Post'
+ *
+ *
+ */
+
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The user's name
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: The user's email
+ *             required:
+ *               - name
+ *               - email
+ *     responses:
+ *       '200':
+ *         description: User successfully registered
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   description: A success message
+ *                 user:
+ *                   $ref: "#/components/schemas/User"
+ *       '400':
+ *         description: Bad Request - Unable to register user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 err:
+ *                   type: string
+ *                   description: Error message details
+ */
+
+
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   post:
+ *     summary: Create a new post
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: User ID
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 description: The post title
+ *               content:
+ *                 type: string
+ *                 description: The post content
+ *             required:
+ *               - title
+ *               - content
+ *     responses:
+ *       '201':
+ *         description: Post successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                   description: The auto-generated id of the post
+ *                 title:
+ *                   type: string
+ *                   description: The post title
+ *                 content:
+ *                   type: string
+ *                   description: The post content
+ *                 user:
+ *                   type: string
+ *                   description: The user ID who created the post
+ *       '400':
+ *         description: Bad Request - Unable to create post
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: "Error message details"
+ *       '404':
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: "User not found"
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: "Internal Server Error"
+ *
+ * /user/{userId}/posts:
+ *   get:
+ *     summary: Get all posts of a user
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         description: User ID
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Successfully retrieved user posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     description: The auto-generated id of the post
+ *                   title:
+ *                     type: string
+ *                     description: The post title
+ *                   content:
+ *                     type: string
+ *                     description: The post content
+ *                   user:
+ *                     type: string
+ *                     description: The user ID who created the post
+ *       '404':
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: "User not found"
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: "Internal Server Error"
+ */
+
+
+
+
+
 
 
 app.post("/register",async (req, res) => {
